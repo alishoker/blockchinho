1d4e77ec3b3dfa1bc5bb419bec49aa5819bac9fc
Ali Shoker <ali.shoker@inesctec.pt>
2018-11-25 13:38:09 +0000

    PoW

---
+++ blockchain/pow.go
@@ -0,0 +1,85 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"math"
+	"math/big"
+	"strconv"
+)
+
+var maxNonce = math.MaxInt64
+
+
+const targetBits = 12
+
+
+type ProofOfWork struct {
+	block *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork{
+
+	target:=big.NewInt(1)
+	target=target.Lsh(target,uint(256-targetBits))
+
+	return &ProofOfWork{b, target}
+	}
+
+func (pow *ProofOfWork) Mine() ( int, []byte){
+
+	//var header [32]byte
+	var (checkHeader big.Int
+	 header [32]byte
+	 nonce  = 0
+	 data []byte
+	)
+
+	preData:=bytes.Join([][]byte{
+		pow.block.PreHeader,
+		[]byte(strconv.FormatInt(int64(pow.block.TimeStamp), 16)),
+		pow.block.Transactions,
+		[]byte(strconv.FormatInt(int64(targetBits), 16))},
+		[]byte{})
+
+	for ; nonce < maxNonce; nonce++ {
+		data=bytes.Join([][]byte{preData,
+			[]byte(strconv.FormatInt(int64(nonce), 16))},
+			[]byte{})
+		header = sha256.Sum256(data)
+
+		//fmt.Printf("nonce: %d, Hash: %x\n", nonce, header)
+		checkHeader.SetBytes(header[:])
+
+		if checkHeader.Cmp(pow.target) == -1 {
+			break
+		}
+		}
+
+
+	return nonce, header[:]
+
+}
+
+func (pow *ProofOfWork) Validate() bool {
+
+	var header [32]byte
+	var checkHeader big.Int
+
+	data:=bytes.Join([][]byte{
+		pow.block.PreHeader,
+		[]byte(strconv.FormatInt(int64(pow.block.TimeStamp), 16)),
+		pow.block.Transactions,
+		[]byte(strconv.FormatInt(int64(targetBits), 16))},
+		[]byte{})
+
+	data=bytes.Join([][]byte{data,
+		[]byte(strconv.FormatInt(int64(pow.block.Nonce), 16))},
+		[]byte{})
+
+		header = sha256.Sum256(data)
+		checkHeader.SetBytes(header[:])
+
+		return checkHeader.Cmp(pow.target) == -1
+}
--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"github.com/alishoker/blockchinho/blockchain"
 	"fmt"
+	"github.com/alishoker/blockchinho/blockchain"
+	"strconv"
 )
 
 func main() {
 
 	bc := blockchain.NewBlockchain()
-	for i:=1;i<5;i++ {
-	bc.AddBlock(fmt.Sprintf("Transactions %d",i))
-      }
+	for i := 1; i < 2; i++ {
+		bc.AddBlock(fmt.Sprintf("Transactions %d", i))
+
+	}
 
 	for nb, block := range bc.Blocks {
 		fmt.Println("========================================")
 		fmt.Printf("Block #: %d\n", nb)
+		fmt.Printf("Nonce #: %d\n", block.Nonce)
 		fmt.Printf("Header: %x\n", block.Header)
 		fmt.Printf("Previous Header: %x\n", block.PreHeader)
 		fmt.Printf("Transactions: %s\n", block.Transactions)
+		pow:=blockchain.NewProofOfWork(block)
+		fmt.Printf("Valid PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 
 }
